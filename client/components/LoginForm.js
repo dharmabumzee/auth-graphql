fd1b9ccd561b4aa0f60ad4ec09ef4f6908862602
lukabelani <luka.belani@gmail.com>
2020-08-02 22:23:08 +0200

    refresh the UI with the current user and catch graphQL errors

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Login';
 import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class LoginForm extends Component {
     onSubmit({ email, password }) {
         this.props.mutate({
-            variables: { email, password }
-        });
+            variables: { email, password },
+            refetchQueries: [{ query }]
+        }).catch(res => { 
+            const errors = res.graphQLErrors.map(error => error.message);
+         });
     }
 
     render() {
