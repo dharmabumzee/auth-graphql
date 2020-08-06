276fa13a8261545cce6a0868e8943244c6f143b5
lukabelani <luka.belani@gmail.com>
2020-08-06 19:52:44 +0200

    create dashboard and auth HOC

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -14,7 +14,7 @@ class LoginForm extends Component {
     
     componentWillUpdate(nextProps) {
         // this.props // current set of props
-        // nextProps // the next set of props that will be in place when component rerenders
+        // nextProps // the next set of props that will be in place when the component rerenders
         if (!this.props.data.user && nextProps.data.user) {
             // redirect to dashboard!!
             hashHistory.push('/dashboard');
